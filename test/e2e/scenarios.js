4b523915b714fdfa8ed122236b68291a474ae0ea
nolan-m <namiller2@gmail.com>
2014-07-07 17:12:08 -0700

    step 3: display query in page

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -3,15 +3,13 @@
 /* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
 
 describe('PhoneCat App', function() {
-
   describe('Phone list view', function() {
 
-    beforeEach(function() {
+    beforeEach(function(){
       browser.get('app/index.html');
     });
 
-
-    it('should filter the phone list as user types into the search box', function() {
+    it('should filter the phone list as user types into the search bax', function(){
 
       var phoneList = element.all(by.repeater('phone in phones'));
       var query = element(by.model('query'));
@@ -22,6 +20,7 @@ describe('PhoneCat App', function() {
       expect(phoneList.count()).toBe(1);
 
       query.clear();
+
       query.sendKeys('motorola');
       expect(phoneList.count()).toBe(2);
     });
