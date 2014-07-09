3f129169de53a19feabdd2e28b6fac0265c4f9be
nolan-m <namiller2@gmail.com>
2014-07-09 16:30:08 -0700

    step 10: swap main image for thumbname when clicked

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -81,7 +81,19 @@ describe('PhoneCat App', function() {
 
     it('should disply four thumbnail iamges on the Nexus S page', function(){
       expect(element.all(by.css('.phone-thumbs li')).count()).toBe(4);
-    })
+    });
 
+    it('should display the first phone image as the main phone image', function(){
+      expect(element(by.css('img.phone')).getAttribute('src')).toMatch(/img\/phones\/nexus-s.0.jpg/);
+    });
+
+    it('should swap main image if a thumbnail image is clicked on', function() {
+      element(by.css('.phone-thumbs li:nth-child(3) img')).click();
+
+      expect(element(by.css('img.phone')).getAttribute('src')).toMatch(/img\/phones\/nexus-s.2.jpg/);
+      element(by.css('.phone-thumbs li:nth-child(1) img')).click();
+
+      expect(element(by.css('img.phone')).getAttribute('src')).toMatch(/img\/phones\/nexus-s.0.jpg/);
+    });
   });
 });
